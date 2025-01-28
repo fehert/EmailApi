a86f452b2e43e9976bb3862ab3d797b14fe36c40
FZoltan40 <farkasz@kkszki.hu>
2025-01-28 10:54:35 +0100

    Email sending is ready.

---
+++ EmailApiKedd/Program.cs
@@ -0,0 +1,40 @@
+
+using EmailApiKedd.Services;
+using EmailApiKedd.Services.IEmail;
+
+namespace EmailApiKedd
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddScoped<IEmailInterface, Email>();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
